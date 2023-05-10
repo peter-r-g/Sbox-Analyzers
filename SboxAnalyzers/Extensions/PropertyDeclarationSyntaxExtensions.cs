8bccd65c2391d8ddb71612ee3771dfaac2e10409
peter-r-g <petergorman11@gmail.com>
2023-05-10 17:30:44 -0400

    Move attribute extensions to MemberDelcarationSyntax

--- SboxAnalyzers/Extensions/PropertyDeclarationSyntaxExtensions.cs
+++ SboxAnalyzers/Extensions/PropertyDeclarationSyntaxExtensions.cs
@@ -10,46 +10,6 @@ namespace SboxAnalyzers.Extensions;
 internal static class PropertyDeclarationSyntaxExtensions
 {
 	/// <summary>
-	/// Tries to get an attribute from the <see cref="PropertyDeclarationSyntax"/>.
-	/// </summary>
-	/// <param name="syntax">The <see cref="PropertyDeclarationSyntax"/> to get the attribute from.</param>
-	/// <param name="attributeName">The name of the attribute.</param>
-	/// <param name="attributeSyntax">The <see cref="AttributeSyntax"/> that was found.</param>
-	/// <returns>Whether or not an <see cref="AttributeSyntax"/> was found.</returns>
-	internal static bool TryGetAttribute( this PropertyDeclarationSyntax syntax, string attributeName, out AttributeSyntax? attributeSyntax )
-	{
-		foreach ( var attributeList in syntax.AttributeLists )
-		{
-			foreach ( var attribute in attributeList.Attributes )
-			{
-				if ( attribute.Name.ToString() != attributeName )
-					continue;
-
-				attributeSyntax = attribute;
-				return true;
-			}
-		}
-
-		// Both "Local" and "LocalAttribute" are valid, need to check both cases.
-		if ( !attributeName.EndsWith( "Attribute" ) )
-			return syntax.TryGetAttribute( attributeName + "Attribute", out attributeSyntax );
-
-		attributeSyntax = null;
-		return false;
-	}
-
-	/// <summary>
-	/// Returns whether or not a <see cref="PropertyDeclarationSyntax"/> has an attribute.
-	/// </summary>
-	/// <param name="syntax">The <see cref="PropertyDeclarationSyntax"/> to check.</param>
-	/// <param name="attributeName">The name of the attribute to find.</param>
-	/// <returns>Whether or not a <see cref="PropertyDeclarationSyntax"/> has the attribute.</returns>
-	internal static bool HasAttribute( this PropertyDeclarationSyntax syntax, string attributeName )
-	{
-		return syntax.TryGetAttribute( attributeName, out _ );
-	}
-
-	/// <summary>
 	/// Returns whether or not a <see cref="PropertyDeclarationSyntax"/> is implemented as an auto-property.
 	/// </summary>
 	/// <param name="syntax">The <see cref="PropertyDeclarationSyntax"/> to check.</param>
